38e94d81f0595a31bc1ed64f17d530d0adae363a
Andrew-Y-Xia <20240026@student.nths.net>
2021-01-15 10:30:57 -0600

    added gen.py

---
+++ gen.py
@@ -0,0 +1,21 @@
+import itertools
+
+def actual(num: int, op: str, num2: int):
+    if op == '+':
+        return num + num2
+    elif op == '-':
+        return num - num2
+    elif op == '*':
+        return num * num2
+    elif op == '/':
+        try:
+            return num / num2
+        except ZeroDivisionError:
+            return 'undefined'
+
+with open("tempfile.txt", 'w') as f:
+    for op in '+-*/':
+        for x, y in itertools.product(range(-100, 100), range(-100, 100)):
+            f.write(f"    if n1 == '{x}' and op == '{op}' and n2 == '{y}':\n        result = '{actual(x, op, y)}'\n")
+            # print(x, y)
+
